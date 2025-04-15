b13336230fe00f32957829c054281d5a68f02d1c
GeorgiyMK <georgiymk82@gmail.com>
2025-04-15 11:59:35 +0300

    first commit

---
+++ DRF/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def demo(request):
+    data = {'message' : 'Привет!'}
+    return Response(data)
